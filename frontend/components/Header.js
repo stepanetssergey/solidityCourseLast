2bba2beb447e683e3879193f95e7da3870176270
NikulinNikita <n_duckhunter@mail.ru>
2022-07-16 23:57:54 +0200

    update sample-test and add removeLiquidityETH, update Header.js

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,7 +4,7 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [3, 58, 137],
+  supportedChainIds: [3, 58, 137, 1337],
 });
 
 export const walletconnect = new WalletConnectConnector({
@@ -21,14 +21,12 @@ const Header = () => {
   const handleConnectWallet = async () => {
     if (!active) {
       await activate(injected, (e) => {
-        error = e.message;
+        let error = e.message;
         if (error?.indexOf("Unsupported chain id") !== -1) {
           deactivate();
           console.log("Unsupported chain");
-          return;
         } else if (error?.indexOf("No Ethereum provider") !== -1) {
           console.log("No ethereum provider");
-          return;
         } else {
           console.log("OK");
         }
@@ -38,9 +36,8 @@ const Header = () => {
 
   const handleConnectWalletWalletConnect = async () => {
     if (!active) {
-      let error;
       await activate(walletconnect, (e) => {
-        error = e.message;
+        console.log(e.message);
       });
     }
   };
@@ -57,19 +54,18 @@ const Header = () => {
       style={{ display: "flex", justifyContent: "space-around" }}
     >
       Active account: {active ? "ACTIVE" : "NOT ACTIVE"}
-      <button onClick={handleConnectWallet}>
-        {active
-          ? account.substring(0, 4) + "..." + account.substring(38, 42)
-          : "Connect Wallet"}
-      </button>
-      <button onClick={handleConnectWalletWalletConnect}>
-        {active
-          ? account.substring(0, 4) + "..." + account.substring(38, 42)
-          : "Connect Wallet WalletConnect"}
-      </button>
-      {active && (
-        <button onClick={handleDisconnectWallet}> Disconnect Wallet </button>
-      )}
+      {!active &&
+        <>
+          <button onClick={handleConnectWallet}>Connect Wallet</button>
+          <button onClick={handleConnectWalletWalletConnect}>Connect Wallet WalletConnect</button>
+        </>
+      }
+      {active &&
+        <>
+          <button>{account.substring(0, 4) + "..." + account.substring(38, 42)}</button>
+          <button onClick={handleDisconnectWallet}> Disconnect Wallet</button>
+        </>
+      }
     </div>
   );
 };
